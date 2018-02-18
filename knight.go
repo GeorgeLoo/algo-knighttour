b1e5939479bba7e292c84d5c2b819676353d4895
George Loo <gloosg@yahoo.com.sg>
2018-02-18 12:39:14 +0800

    backtracking working
    but global sol

--- knight.go
+++ knight.go
@@ -13,7 +13,114 @@ knight tour puzzle  17.2.2018 George Loo
 package main
  
 import "fmt"
+
+const (
+	N = 8
+	
+)
+
+var (
+	sol[N][N] int
+)
+
+/* A utility function to check if i,j are valid indexes
+   for N*N chessboard */
+func isSafe(x int,  y int, sol[N][N] int) bool {
+	ret := ( x >= 0 && x < N && y >= 0 &&
+          y < N && sol[x][y] == -1)
+    return ret
+}
+
+/* A utility function to print solution matrix sol[N][N] */
+func printSolution( sol[N][N] int) {
+    for x := 0; x < N; x++ { 
+    
+        for y := 0; y < N; y++ {
+            fmt.Printf(" %2d ", sol[x][y]);
+        }
+        fmt.Printf("\n");
+    }
+}
+
+
+/* This function solves the Knight Tour problem using
+   Backtracking.  This function mainly uses solveKTUtil()
+   to solve the problem. It returns false if no complete
+   tour is possible, otherwise return true and prints the
+   tour.
+   Please note that there may be more than one solutions,
+   this function prints one of the feasible solutions.  */
+func solveKT() bool {
+
+	//var xMove[8] int
+	//var yMove[8] int
+
+    /* Initialization of solution matrix */
+    for x := 0; x < N; x++ {
+        for y := 0; y < N; y++ {
+            sol[x][y] = -1;
+        }
+    }
+
+    /* xMove[] and yMove[] define next move of Knight.
+       xMove[] is for next value of x coordinate
+       yMove[] is for next value of y coordinate */
+       //s := []int{5, 2, 6, 3, 1, 4}
+    xMove := []int{  2, 1, -1, -2, -2, -1,  1,  2 }
+    yMove := []int{  1, 2,  2,  1, -1, -2, -2, -1 }
+
+ 
+    // Since the Knight is initially at the first block
+    sol[0][0] = 0;
+    //sol[7][7] = 37
+   /* Start from 0,0 and explore all tours using
+       solveKTUtil() */
+    if (solveKTUtil(0, 0, 1, xMove, yMove) == false)   {
+        fmt.Printf("Solution does not exist\n");
+        return false
+    }    else {
+        printSolution(sol)
+    }
+
+
+    return true
+}
+
+/* A recursive utility function to solve Knight Tour
+   problem */
+
+func solveKTUtil(x int, y int, movei int ,xMove[] int, yMove[] int) bool {
+   var  k, next_x, next_y int
+
+
+   if movei == N*N {
+   		fmt.Printf("when 64\n");
+       	return true
+   }
+
+
+ 
+   /* Try all next moves from the current coordinate x, y */
+   for k = 0; k < 8; k++   {
+       next_x = x + xMove[k]
+       next_y = y + yMove[k]
+       if isSafe(next_x, next_y, sol) {
+         	sol[next_x][next_y] = movei
+         	if solveKTUtil(next_x, next_y, movei+1, xMove, yMove) == true {
+             	return true
+       		} else {
+       			sol[next_x][next_y] = -1// backtracking
+       		}
+       }
+   }
+ 
+   return false
+}
+
  
 func main() {
-  fmt.Printf("Hello world!\n")
+  	fmt.Printf("Knight Tour Puzzle!\n")
+  	//sol[1][1] = -1
+	//printSolution(sol)
+	solveKT()
 }
